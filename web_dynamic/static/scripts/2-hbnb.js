db49489f0377030589c5c72f145318ef1b4374a9
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-13 00:52:53 +0000

    Removing every content from tags <article> and making the changes in the script so it can post the same information plus ignoring user/owner tags and updating routes in python scripts flask

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
   const amenityCheck = {};
   $('.amenities INPUT').change(function () {
-        if (this.checked) {
-          amenityCheck.push($(this).attr('data-name'))
-	} else {
-	   amenityCheck.pop($(this).attr('data-name'));
-	}
-	$('amenities H4').html(Object.values(amenityCheck).join(', '));
+    if (this.checked) {
+      amenityCheck.push($(this).attr('data-name'));
+    } else {
+      amenityCheck.pop($(this).attr('data-name'));
+    }
+    $('amenities H4').html(Object.values(amenityCheck).join(', '));
   });
-    $.get("http://0.0.0.0:5001/api/v1/status", function  (data) {
-	if (data.status === 'success')
-	    $("#api_status").addClass('available');
-	else
-	    $("#api_status").removeClass('available');
-    });
+  $.get('http://0.0.0.0:5001/api/v1/status', function (data) {
+    if (data.status === 'success') { $('#api_status').addClass('available'); } else { $('#api_status').removeClass('available'); }
+  });
 });
